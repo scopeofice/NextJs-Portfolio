55118981f818a2c0d6c3137a7205557a224b3b57
Shubham Ghodkhande <shubhamg0615@gmail.com>
2023-10-29 23:29:28 +0530

    for deployment

--- src/pages/about.js
+++ src/pages/about.js
@@ -51,19 +51,20 @@ export default function about() {
             <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8'>
               <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>About me</h2>
               <p className='font-medium'>
-                Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional,
-                and user-centered digital experiences. With 4 years of experience in the field. I am always looking for
-                new and innovative ways to bring my clients' visions to life.
-              </p>
-              <p  className='font-medium my-4'>
-                I believe that design is about more than just making things look pretty. it's about solving problems and
-                creating intuitive, enjoyable experiences for users.
-              </p>
-              <p className='font-medium'>
-                Whether I'm working on a website, mobile app, or
-                other digital product, I bring my commitment to design excellence and user-centered thinking to
-                every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
-              </p>
+  Hi, I&apos;m CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional,
+  and user-centered digital experiences. With 4 years of experience in the field. I am always looking for
+  new and innovative ways to bring my clients&apos; visions to life.
+</p>
+<p  className='font-medium my-4'>
+  I believe that design is about more than just making things look pretty. it&apos;s about solving problems and
+  creating intuitive, enjoyable experiences for users.
+</p>
+<p className='font-medium'>
+  Whether I&apos;m working on a website, mobile app, or
+  other digital product, I bring my commitment to design excellence and user-centered thinking to
+  every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
+</p>
+
             </div>
 
             <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:col-span-8 md:order-1'>
