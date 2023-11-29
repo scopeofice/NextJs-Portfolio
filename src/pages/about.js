c1c5d076fe5278677fa5d10c932e22428a66725b
Shubham Ghodkhande <130497256+scopeofice@users.noreply.github.com>
2023-11-29 17:49:13 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -51,7 +51,7 @@ export default function about() {
             <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8'>
               <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>About me</h2>
               <p className='font-medium'>
-  Hi, I&apos;m Shubham Ghodkhande, a web developer and UI/UX designer with a passion for creating beautiful, functional,
+  Hi, I&apos;m Mukesh Kore, a web developer and UI/UX designer with a passion for creating beautiful, functional,
   and user-centered digital experiences. With 4 years of experience in the field. I am always looking for
   new and innovative ways to bring my clients&apos; visions to life.
 </p>
